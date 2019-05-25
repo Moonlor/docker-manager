317f1cc47f8b4efee42b8dd98f1d11809aaa2cdf
Moonlor <kenneth0875@gmail.com>
2019-05-25 15:06:05 +0800

    [ADD] Frontend - Add request util

--- DockerMgrFrontEnd/src/layouts/index.js
+++ DockerMgrFrontEnd/src/layouts/index.js
@@ -1,12 +1,77 @@
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Component } from 'react';
+
 import styles from './index.css';
 
-function BasicLayout(props) {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      {props.children}
-    </div>
-  );
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
+
+class MainLayout extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false,
+    };
+  }
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  render() {
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+          <div className={styles.logo} />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="user" />
+              <span>User</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Team</span>
+                </span>
+              }
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span>File</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          {/* <Header style={{ background: '#fff', padding: 0 }} title="Title" subTitle="This is a subtitle" /> */}
+          <Content style={{ margin: '0 16px' }}>
+            {/* <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb> */}
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              {this.props.children}
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Docker Manager ©2019 Created by Moonlor</Footer>
+        </Layout>
+      </Layout>
+    );
+  }
 }
 
-export default BasicLayout;
+export default MainLayout;
