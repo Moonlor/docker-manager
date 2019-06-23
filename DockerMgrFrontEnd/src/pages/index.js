52799c9b8cc9eb51bc9ba48c32e68773d048f20a
Moonlor <kenneth0875@gmail.com>
2019-06-24 01:37:03 +0800

    [UPDATE] Update login welcome image

--- DockerMgrFrontEnd/src/pages/index.js
+++ DockerMgrFrontEnd/src/pages/index.js
@@ -5,9 +5,9 @@ export default function() {
   return (
     <div className={styles.normal}>
       <div className={styles.welcome} />
-      <ul className={styles.list}>
+      {/* <ul className={styles.list}>
         <li>欢迎来到Docker容器管理平台</li>
-      </ul>
+      </ul> */}
     </div>
   );
 }
