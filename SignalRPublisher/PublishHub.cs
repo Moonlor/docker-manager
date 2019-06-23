e42eaf0b34b5845d3d974bff1ce738d9f60fcece
Moonlor <kenneth0875@gmail.com>
2019-06-24 01:41:07 +0800

    [ADD] Add signalR module -- depreciated

---
+++ SignalRPublisher/PublishHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRPublisher
+{
+    public class PublishHub : Hub
+    {
+        public async Task SendToAll(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
