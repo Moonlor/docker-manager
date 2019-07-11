7a227db987f9d8a902c80c896e9e984ef7ee87df
Moonlor <kenneth0875@gmail.com>
2019-07-11 17:00:11 +0800

    [FIX] Resolve conflicts

--- DockerMgrFrontEnd/src/utils/constants.js
+++ DockerMgrFrontEnd/src/utils/constants.js
@@ -1,6 +1,6 @@
-const DOMAIN = 'http://http://111.231.143.45/:5000'
+const DOMAIN = 'http://111.231.143.45:5010'
 const VERSION = 'v1.0'
 
-const WS_DOMAIN = 'ws://http://111.231.143.45/:5000'
+const WS_DOMAIN = 'ws://111.231.143.45:5010'
 
 export { DOMAIN, VERSION, WS_DOMAIN }
